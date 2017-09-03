069acd16aa76b91d5b420a85639f9e7a21c9eb09
lucasnnobrega <lucasnnobrega@gmail.com>
2017-09-03 20:33:24 -0300

    Agora Tudo OK

---
+++ Atividade 1/Atividade-1 PY3/sequencial.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 22 17:45:10 2017
+
+@author: lucasnn
+"""
+# Programa sequencial que calcula uma matriz ao quadrado(AxA = C)
+
+import numpy as np
+
+ORDEM = 250
+
+matriz_aleat = np.random.rand(ORDEM,ORDEM)
+
+resultado = np.zeros((ORDEM, ORDEM))
+
+resultado = np.matmul(matriz_aleat, matriz_aleat)
+"""
+# itera sobre as linhas X
+for i in range(len(matriz_aleat)):
+   # itera sobre as colunas de Y
+   for j in range(len(matriz_aleat[0])):
+       # itera sobre as linhas de Y
+       for k in range(len(matriz_aleat)):
+           resultado[i][j] += matriz_aleat[i][k] * matriz_aleat[k][j]
+           #print(i, j, k)
+"""
+for linha in resultado:
+   print(linha)
---
+++ README.md
@@ -0,0 +1,9 @@
+# Aulas-LP2
+Códigos para a cadeira de Linguagem de Programação 2
+
+#### Atividade-1
+Comparação entre uma multiplicação sequencial e com o uso de threads.
+
+#### Exercício de Programação 1
+Utiliza o conceito de algorítimo justos(Da Padaria) para simular um parque de 
+diversões.
